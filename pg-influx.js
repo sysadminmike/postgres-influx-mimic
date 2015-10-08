837dca05753960a1e974ddb692597a8fd20af8be
sysadminmike <mike@wolman.co.uk>
2015-10-08 21:15:45 +0100

    initial commit

---
+++ pg-influx.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+
+var pg = require('pg');
+
+var conString = "postgres://mike:highgate22@192.168.3.22/couchplay";
+
+
+pgclient = new pg.Client(conString);
+
+pgclient.connect(function(err) {
+            if (err) {
+                console.error('ERROR: Could not connect to postgres', err);
+                process.exit();
+            } else {
+                console.log('Connected to postgres');
+            }
+});
+
+
+
+var http = require("http");
+var url = require("url") ;
+var control_port = 8086;
+
+
+function onRequest(request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+//    console.log(request.url);
+
+    var queryObject = url.parse(request.url,true);
+//    console.log(queryObject);
+
+
+    switch (queryObject.pathname) {
+        case '/query':
+
+            response.write("Pretend to be influx\n");            
+            response.write("SQL: " + queryObject.query.q + "\n"); 
+
+            console.log(queryObject.query.q);
+
+            pgclient.query(queryObject.query.q, function(err, result) {
+                if (err) {
+                    console.error('ERROR: ', err);
+                }else{
+//                  console.error('ret: ', result.rows[0].ret);
+                    response.write( String(result.rows[0].ret) );           
+                }
+                response.end();
+
+            });
+            break;
+        default:            
+            response.write("OK\n");
+            response.write(request.url);
+            response.end();
+    }
+}
+
+http.createServer(onRequest).listen(control_port);
+console.log('Listening on port ' + control_port);
